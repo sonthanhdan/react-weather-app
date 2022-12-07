f7746b2a6eead5bbc55f251ab027b7c99b9f9dda
Son Thanh Dan <thanhdan1995@gmail.com>
2022-12-07 17:44:18 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.9e1077acfd281cb5417c19197829c0f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ebbabf8a79d1aec48cca647d6040ae",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "4bc04b238eac88c34fe8",
+    "url": "/react-weather-app/static/css/main.38da8901.chunk.css"
+  },
+  {
+    "revision": "1a98a69ecb58d8af4949",
+    "url": "/react-weather-app/static/js/2.2c8de82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.2c8de82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc04b238eac88c34fe8",
+    "url": "/react-weather-app/static/js/main.9a5e2f4e.chunk.js"
+  },
+  {
+    "revision": "13a2975eef68b2429ce8",
+    "url": "/react-weather-app/static/js/runtime-main.5e4acc94.js"
+  },
+  {
+    "revision": "2090097b8974095c85d0b45d804432e0",
+    "url": "/react-weather-app/static/media/Pe-icon-7-weather.2090097b.svg"
+  },
+  {
+    "revision": "96ed4a038b463b1ec77333dc95bc5cce",
+    "url": "/react-weather-app/static/media/Pe-icon-7-weather.96ed4a03.ttf"
+  },
+  {
+    "revision": "c18a51f85f51e3ee98336815b91b00de",
+    "url": "/react-weather-app/static/media/Pe-icon-7-weather.c18a51f8.eot"
+  },
+  {
+    "revision": "cc8e85eade2c4a331c6fd5eb998156f5",
+    "url": "/react-weather-app/static/media/Pe-icon-7-weather.cc8e85ea.woff"
+  }
+]);
